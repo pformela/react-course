2455b0cca13b9bc64dc1e94f585d4caeba88c676
pformela <patrykformela.00@gmail.com>
2022-10-16 23:13:28 +0200

    complete practice project, fragments, portals and refs

--- 03-practice-project/src/App.js
+++ 03-practice-project/src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: prevUsersList.length + 1 },
+      ];
+    });
+  };
+
   return (
-    <div>
-      <AddUser />
-    </div>
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
   );
 }
 
